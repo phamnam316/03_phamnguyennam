85e01d32f4a390c7704b1b6605e4298cb3cec7aa
phamnam316 <phamnam316.np@gmail.com>
2023-08-02 19:50:35 +0700

    update

--- lesson_1-1.sql
+++ lesson_1-1.sql
@@ -3,87 +3,87 @@ Create Database `Testing_System_Assignment_1`;
 Use `Testing_System_Assignment_1`;
 
 Drop Table if exists `table_Department`;
-Create Table `table_Department`(
-  DepartmentID  INT,
-  DepartmentIDName VARCHAR(50)
+CREATE TABLE `table_Department` (
+    DepartmentID INT,
+    DepartmentIDName VARCHAR(50)
 );
 
-Drop Table if exists `table_Position`; 
-Create Table `table_Position`(
-  PositionID    INT,
-  PositionName  VARCHAR(20)
+Drop Table if exists `table_Position`;
+CREATE TABLE `table_Position` (
+    PositionID INT,
+    PositionName VARCHAR(20)
 );
 
-Drop Table if exists `table_Account`; 
-Create Table `table_Account`( 
-  AccountID    INT,
-  Email        VARCHAR(50),
-  UserName     VARCHAR(50),
-  FullName     VARCHAR(50),
-  DepartmentID  INT,
-  PositionID    INT,
-  CreateDate    DATE
+Drop Table if exists `table_Account`;
+CREATE TABLE `table_Account` (
+    AccountID INT,
+    Email VARCHAR(50),
+    UserName VARCHAR(50),
+    FullName VARCHAR(50),
+    DepartmentID INT,
+    PositionID INT,
+    CreateDate DATE
 );
 
 
-Drop Table if exists `table_Group`; 
-Create Table `table_Group`( 
-  GroupID    INT,
-  GroupName    VARCHAR(50),
-  CreatorID  INT,
-  CreateDate    DATE
+Drop Table if exists `table_Group`;
+CREATE TABLE `table_Group` (
+    GroupID INT,
+    GroupName VARCHAR(50),
+    CreatorID INT,
+    CreateDate DATE
 );
 
-Drop Table if exists `table_GroupAccount`; 
-Create Table `table_GroupAccount`( 
-  GroupID    INT,
-  AccountID  INT,
-  JoinDate    DATE
+Drop Table if exists `table_GroupAccount`;
+CREATE TABLE `table_GroupAccount` (
+    GroupID INT,
+    AccountID INT,
+    JoinDate DATE
 );
 
-Drop Table if exists `table_TypeQuestion`; 
-Create Table `table_TypeQuestion`( 
-  TypeID    INT,
-  TypeName  VARCHAR(50)
+Drop Table if exists `table_TypeQuestion`;
+CREATE TABLE `table_TypeQuestion` (
+    TypeID INT,
+    TypeName VARCHAR(50)
 );
 
-Drop Table if exists `table_CategoryQuestion`; 
-Create Table `table_CategoryQuestion`( 
-  CategoryID    INT,
-  CategoryName  VARCHAR(50)
+Drop Table if exists `table_CategoryQuestion`;
+CREATE TABLE `table_CategoryQuestion` (
+    CategoryID INT,
+    CategoryName VARCHAR(50)
 );
 
-Drop Table if exists `table_Question`; 
-Create Table `table_Question`( 
-  QuestionID    INT,
-  Content     VARCHAR(200),
-  CategoryID    INT,
-  TypeID        INT,
-  CreatorID     INT,
-  CreateDate    DATE
+Drop Table if exists `table_Question`;
+CREATE TABLE `table_Question` (
+    QuestionID INT,
+    Content VARCHAR(200),
+    CategoryID INT,
+    TypeID INT,
+    CreatorID INT,
+    CreateDate DATE
 );
 
-Drop Table if exists `table_Answer`; 
-Create Table `table_Answer`( 
-  AnswerID    INT,
-  Content     VARCHAR(200),
-  QuestionID  INT,
-  isCorrect    VARCHAR(50)
+Drop Table if exists `table_Answer`;
+CREATE TABLE `table_Answer` (
+    AnswerID INT,
+    Content VARCHAR(200),
+    QuestionID INT,
+    isCorrect VARCHAR(50)
 );
 
-Drop Table if exists `table_Exam`; 
-Create Table `table_Exam`( 
-  ExamID    INT,
-  Code      INT,
-  Title     VARCHAR(100),
-  CategoryID    INT,
-  Duration      TIME,
-  CreatorID     INT,
-  CreateDate    DATE
-);
+Drop Table if exists `table_Exam`;
+CREATE TABLE `table_Exam` (
+    ExamID INT,
+    Code INT,
+    Title VARCHAR(100),
+    CategoryID INT,
+    Duration TIME,
+    CreatorID INT,
+    CreateDate DATE
+);  
 
-Drop Table if exists `table_ExamQuestion`; 
-Create Table `table_ExamQuestion`( 
-  ExamID    INT,
-  QuestionID    INT
+Drop Table if exists `table_ExamQuestion`;
+CREATE TABLE `table_ExamQuestion` (
+    ExamID INT,
+    QuestionID INT
 );
--- lesson_1.sql
+++ lesson_1.sql
@@ -4,17 +4,17 @@ USE `Fresher Training Management`;
 -- Exercise 1:
 -- Question 1:	
 DROP TABLE IF EXISTS `Table_Trainee`;
-CREATE TABLE `Table_Trainee`(
- TraineeID  INT AUTO_INCREMENT,
-  Full_Name  VARCHAR(50),
-  Birth_Date DATE,
-  Gener VARCHAR(10),
-  ET_IQ  INT,
-  ET_Gmath INT,
-  ET_English INT,
-  Training_Class INT,
-  Evaluation_Notes TEXT,
-  PRIMARY KEY(TraineeID)
+CREATE TABLE `Table_Trainee` (
+    TraineeID INT AUTO_INCREMENT,
+    Full_Name VARCHAR(50),
+    Birth_Date DATE,
+    Gener VARCHAR(10),
+    ET_IQ INT,
+    ET_Gmath INT,
+    ET_English INT,
+    Training_Class INT,
+    Evaluation_Notes TEXT,
+    PRIMARY KEY (TraineeID)
 );
 
 -- Question 2:
@@ -24,23 +24,23 @@ ALTER TABLE `Table_Trainee` ADD VTI_Account VARCHAR(50) NOT NULL UNIQUE;
 -- Exercise 2:
 
 DROP TABLE IF EXISTS `Data_Type`;
-CREATE TABLE `Data_Type`(
-ID INT AUTO_INCREMENT,
-Name VARCHAR(50),
-Code VARCHAR(5),
-ModifiedDate DATETIME,
-PRIMARY KEY(ID)
+CREATE TABLE `Data_Type` (
+    ID INT AUTO_INCREMENT,
+    Name VARCHAR(50),
+    Code VARCHAR(5),
+    ModifiedDate DATETIME,
+    PRIMARY KEY (ID)
 );		
 
 -- Exercise 3:
 
 DROP TABLE IF EXISTS `Data_Type_2`;
-CREATE TABLE `Data_Type_2`(
-ID INT AUTO_INCREMENT,
-Name VARCHAR(50),
-BirthDate DATE,
-Gender INT,
-IsDeletedFlag BIT,
-PRIMARY KEY(ID)
+CREATE TABLE `Data_Type_2` (
+    ID INT AUTO_INCREMENT,
+    Name VARCHAR(50),
+    BirthDate DATE,
+    Gender INT,
+    IsDeletedFlag BIT,
+    PRIMARY KEY (ID)
 );
 		
